aafdd87ef1147b7baaf7c0fb5bf6889e0d5b539b
Aslan-mu <35272557+Aslan-mu@users.noreply.github.com>
2019-10-12 14:41:42 -0400

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default{
-    apiKey: "AIzaSyBLZEwMES6yfSuJ5ItC192dBZJ2ptSHc8Y",
-    authDomain: "pawsomecompanions-21c1b.firebaseapp.com",
-    databaseURL: "https://pawsomecompanions-21c1b.firebaseio.com",
-    projectId: "pawsomecompanions-21c1b",
-    storageBucket: "pawsomecompanions-21c1b.appspot.com",
-    messagingSenderId: "86684595211",
-    appId: "1:86684595211:web:aa22461d913fcf7103430e"
-  }
